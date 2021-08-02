979d27f1b30fd409c7d738bfccb594c099f58adb
yuliyag <mc.ygalperina@gmail.com>
2021-08-02 11:23:46 -0700

    config update

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -28,7 +28,7 @@ const fs = require('fs-extra')
 const path = require('path')
 
 function getConfigurationByFile (file) {
-    const pathToConfigFile = path.resolve('../e2e', 'config', `${file}.json`)
+    const pathToConfigFile = path.resolve('../scdemo', 'config', `${file}.json`)
     return fs.readJson(pathToConfigFile)
 }
 
